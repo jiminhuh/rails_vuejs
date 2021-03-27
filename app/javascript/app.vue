76a410b9cc3f09eacbdf2781938fc9c4a29b17ec
Jimin Huh <jiminhuh95@gmail.com>
2021-03-27 10:19:23 -0500

    vue components, db schema

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,15 +1,24 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
+    <Users />
   </div>
 </template>
 
 <script>
+
+import Users from './components/Users.vue'
+
 export default {
+  name: "App",
+  components: {
+    Users
+  },
   data: function () {
     return {
-      message: "Hello Vue!"
+      message: "Hello!"
     }
+
   }
 }
 </script>
---
+++ app/javascript/components/Users.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="User">
+    <h1>Jimin Huh</h1>
+    <h1>jiminhuh95@gmail.com</h1>
+    <h1>(512)497-5831</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
